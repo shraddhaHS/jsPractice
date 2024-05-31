8bce57636145f2147e433b12fe4d8d4c7112a607
shraddhaHS <88781426+shraddhaHS@users.noreply.github.com>
2024-05-31 19:08:18 +0000

    operations done

---
+++ 01basics/operations.js
@@ -0,0 +1,22 @@
+let value=3
+let negValue = -value
+console.log(negValue)
+console.log(6/3); //division
+console.log(5%2);// remainder
+let str1= "shra"
+let str2 = "hul"
+let str3 = str1 + str2 //concat
+console.log(str3)
+console.log("1"+2) // output 12
+console.log(1+2) // output 3
+console.log(1+"2") // output 12
+console.log("1"+ 2+3)// output  123 pehle string aayi toh sb concat and conversion to string krdenge
+console.log(1+2+"2")//output 32 pehle 1+2=3 then concat and conversion to string
+console.log(+true) //increment kia toh conversion hua nd output 1
+console.log(+"") // output 0
+
+
+let gameCounter = 100;
+// ++gameCounter incremented by 1 but afterwards as it i postfix
+//gameCounter++ // incremented by 1 101 prefix mai pehle increment ho jati h postfix mai baadme
+console.log(gameCounter)
