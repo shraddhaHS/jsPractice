dddeb96848714795f5440678a7d3365248df79cc
shraddhaHS <88781426+shraddhaHS@users.noreply.github.com>
2024-05-31 19:43:56 +0000

    summary

--- 01basics/datatype.js
+++ 01basics/datatype.js
@@ -1,9 +1,9 @@
- "use strict"; // treat all js code as newer version
-//  alert(3+3)  yeh browser mai console mai run kr jaega pr node mai nhi
-// console.log(3+3); console.log("shraddha") dont write your code like this make it readable use next line
- let name = "shraddha" //string ""
- let age= 21 //number range(2^53) bigInt
-let isLoggenIn = false //boolean datatype
+//  "use strict"; // treat all js code as newer version
+// //  alert(3+3)  yeh browser mai console mai run kr jaega pr node mai nhi
+// // console.log(3+3); console.log("shraddha") dont write your code like this make it readable use next line
+//  let name = "shraddha" //string ""
+//  let age= 21 //number range(2^53) bigInt
+// let isLoggenIn = false //boolean datatype
 
 //null => standalone value and it is a object in js/ representation of empty value
 //undefined =>variable declare kia h pr usko koi value nhi di
@@ -29,4 +29,39 @@ let isLoggenIn = false //boolean datatype
 
 //IMPORTANT
 // strict check === sirf values nhi unke datatype ko bhi check krta hai
-console.log("2"===2) //will give false as output bcoz datatype is different
+// console.log("2"===2) //will give false as output bcoz datatype is different
+
+// DATATYPES SUMMARIZATION
+// primitive datatypes(7): call by value not by reference (value ki copy ke saath deal krte h na ki address)
+//String,Number,Boolean,Null,undefined,Symbol, BigInt
+
+
+//point to be noted my lord: javascript is dynamically typed language variables ka type defined krte agr krna hota for safety toh woh statically typed ho jata
+const score= 100
+const scoreValue= 100.3 //yeh bhi number hai kutch float etc nhi hota js mai
+const outsideTemp = null // dattaype of outsideTemp is object
+console.log(typeof outsideTemp) 
+const isLoggedIn= false
+let userEmail; //undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id===anotherId)
+//symbol aisa datatype hai jo value ko unique bna deta hai even if value apparently same ho pr symbol ki help se they will be different nd unique
+const bigNumber = 2345678984567789n
+// console.log(typeof bigNumber) ->bigInt
+
+//Non-primitive : reference (memory mai reference allocate kia jata hai)
+
+//array,Objects,Functions 
+
+const heros = ["shaktiman","spideyboy","eren"] //array 
+let myObj = { //objects
+    name:"ssb",
+    age : 32
+
+}
+const myFunction = function(){
+console.log("hello earth")
+}
+ console.log(typeof myFunction) // return object function reference wale mai sbka type object hi aata hai
