b8eb4a7aa15dffe2494366b34e49fe5818a0dd3c
shraddhaHS <88781426+shraddhaHS@users.noreply.github.com>
2024-06-07 12:40:08 +0000

    commit

---
+++ 05Iterations/seven.js
@@ -0,0 +1,9 @@
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+// const newNums = myNums.map((num)=> num+10)
+//chaining map krneke baad jjo output aaya upe phir map lgao ya filter lgao sb possible h
+// const newNums = myNums.map((num)=> num*10).map((num)=> num+1 ).filter((num)=> num>=40)
+const newNums = myNums
+            .map((num)=> num*10)
+            .map((num)=> num+1 )
+            .filter((num)=> num>=40)
+console.log(newNums)
