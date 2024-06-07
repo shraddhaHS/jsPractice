6ca16d5736d55432ff1ac83a17980fd0308521a0
shraddhaHS <88781426+shraddhaHS@users.noreply.github.com>
2024-06-07 08:50:41 +0000

    commit

---
+++ 05Iterations/two.js
@@ -0,0 +1,27 @@
+// while (condition) {
+    
+// }  kb tk condition met hoti h tb tk run hoga
+// let index = 0
+// while(index<=10){
+//     console.log(`value of index is ${index}`)
+//     index = index + 2
+// }
+// let myArr = ["flash","batman","perman"]
+// let arr = 0
+// while(arr<myArr.length){
+//     console.log(`value is ${myArr[arr]}`)
+//     arr = arr+1
+// }
+// let score = 1
+// do{
+//  console.log(`score is ${score}`)
+//  score++
+
+// } while(score <=10)
+// let score = 11
+// do{
+//  console.log(`score is ${score}`)
+//  score++
+
+// } while(score <=10)
+    // iska output 11 aaega as yahaan pehle kaam hota h phir condition check krte hain
