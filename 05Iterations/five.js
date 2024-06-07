216f00a5bd3a4041d9b1d851d9e82673428004ae
shraddhaHS <88781426+shraddhaHS@users.noreply.github.com>
2024-06-07 12:30:46 +0000

    commit

---
+++ 05Iterations/five.js
@@ -0,0 +1,44 @@
+const coding = ["js","rb","c++","python","java"]
+//foreach
+//callback function hoga uska naam nhi hota
+// coding.forEach( function (item){
+//     console.log(item)
+// }   )
+//using arrow callback function
+// coding.forEach( (item) => { console.log(item)}  ) 
+
+// function printMe(item){
+//     console.log(item)
+// }
+// //using this function as callback
+// coding.forEach(printMe)
+
+// coding.forEach((item,index,arr)=> {
+//     console.log(item,index,arr)
+// })
+// js 0 [ 'js', 'rb', 'c++', 'python', 'java' ]
+// rb 1 [ 'js', 'rb', 'c++', 'python', 'java' ]
+// c++ 2 [ 'js', 'rb', 'c++', 'python', 'java' ]
+// python 3 [ 'js', 'rb', 'c++', 'python', 'java' ]
+// java 4 [ 'js', 'rb', 'c++', 'python', 'java' ]
+
+// [{},{},{}]
+const myCoding = [
+    {
+        languageName : "javascript",
+        filename:"js"
+    },
+    {
+        languageName : "java",
+        filename:"java"
+    },
+    {
+        languageName : "python",
+        filename:"py"
+    },
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageName)
+    // console.log(item.filename)
+})
---
+++ 05Iterations/six.js
@@ -0,0 +1,38 @@
+// const coding = ["js","rb","c++","python","java"]
+// const values = coding.forEach( (item)=> {
+// //    console.log(item)
+// //    return item //forEacg doesnt return any value
+// })
+// console.log(values)
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+// let newNums= myNums.filter( (num) => num > 4 )
+// let newNums= myNums.filter( (num) => {
+//     return num>4
+// } )
+// const newNums = []
+// myNums.forEach( (num) => {
+//     if (num>4) {
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums) //output [5,6,7,8,9,10]
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+//filters this is how we filter in our websites too
+// arrow function single line mai jb bina curly braces khole without blockscope initiate krke likhte h toh return algse nhi likhna pdta but agr curly braces mai method likh rhe h toh return krna pdega wrna answer nhi aaega
+// const userBooks = books.filter( (book)=> book.genre ==='History' )
+const userBooks = books.filter( (book) => {
+    return  book.publish >=1995 && book.genre === "History"
+ })
+console.log(userBooks)
